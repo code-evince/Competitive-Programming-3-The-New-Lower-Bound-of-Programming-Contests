d81daecef0f7dfffbce600c800c28475030c851c
Bishnu <38295546+code-evince@users.noreply.github.com>
2020-04-13 17:03:58 +0530

    Update 12250 - Language Detection.py

--- Introduction/Getting Started : The Easy Problems/Super Easy/12250 - Language Detection.py
+++ Introduction/Getting Started : The Easy Problems/Super Easy/12250 - Language Detection.py
@@ -1,1 +1,13 @@
+i=1
+while(True):
+    text = input()
+    if(text == '#'):
+        break
+    language = {"HELLO":"ENGLISH","HOLA":"SPANISH","HALLO":"GERMAN","BONJOUR":"FRENCH","CIAO":"ITALIAN","ZDRAVSTVUJTE":"RUSSIAN"}
+    if(text in language):
+        print("Case {}: {}".format(i,language[text]))
+        i+=1
+    else:
+        print("Case {}: UNKNOWN".format(i))
+        i=i+1
 
