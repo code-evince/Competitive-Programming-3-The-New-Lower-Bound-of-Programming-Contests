ce8f8659a69c1e513d5628cab6b259ee9b4e7d4b
Bishnu <38295546+code-evince@users.noreply.github.com>
2020-04-13 16:49:36 +0530

    Update 272 - TEX Quotes.py

--- Introduction/Getting Started : The Easy Problems/Super Easy/272 - TEX Quotes.py
+++ Introduction/Getting Started : The Easy Problems/Super Easy/272 - TEX Quotes.py
@@ -1,1 +1,49 @@
+check=0
+while(True):
+    try:
+        x = input()
+    except EOFError:
+        break
 
+    a=[]
+    for i in x:
+        if(i=='\"'):
+            if(check%2==0):
+                a.append('``')
+                check=check+1
+            else:
+                a.append("''")
+                check=check+1
+        else:
+            a.append(i)
+
+    print("".join(a))
+
+
+'''
+OR USING FLAG
+The EOFError is crucial for ensuring multiple testcases when no. of testcases
+aren't given
+
+check=0
+while(True):
+    try:
+        x = input()
+    except EOFError:
+        break
+
+    a=[]
+    for i in x:
+        if(i=='\"'):
+            if(check==0):
+                a.append('``')
+                check=1
+            else:
+                a.append("''")
+                check=0
+        else:
+            a.append(i)
+
+    print("".join(a))
+
+    '''
